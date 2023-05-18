19cfe7a388990c56a67efedf54356ddcba4f5a39
hunglv98 <hungle060419982@gmail.com>
2023-05-18 20:41:39 +0700

    nop bai ngay 18/05

--- case_study/case_study.sql
+++ case_study/case_study.sql
@@ -189,13 +189,20 @@ values (5,2,4),
 (2,1,2),
 (2,12,2);
 
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+
 select ma_nhan_vien,ho_ten
 from nhan_vien
 where (ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%') and char_length(ho_ten)<16;
 
+-- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+
 select ma_khach_hang,ho_ten
 from khach_hang
 where (dia_chi like '%Đà Nẵng%' or dia_chi like '%Quảng Trị%') and (timestampdiff(year,ngay_sinh,curdate())>=18 and (timestampdiff(year,ngay_sinh,curdate())) <= 50);
+
+-- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
 
 select kh.ma_khach_hang, kh.ho_ten, count(kh.ma_khach_hang)
 from hop_dong hd
@@ -207,19 +214,9 @@ where lk.ten_loai_khach = "Diamond"
 group by ma_khach_hang 
 order by count(kh.ma_khach_hang);
 
-select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong,dv.ten_dich_vu, hd.ngay_lam_hop_dong,hd.ngay_ket_thuc, (dv.chi_phi_thue+dvdk.gia*hdct.so_luong) as total
-from dich_vu_di_kem dvdk
-join hop_dong_chi_tiet hdct
-on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem
-join hop_dong hd
-on hd.ma_hop_dong = hdct.ma_hop_dong
-join dich_vu dv
-on dv.ma_dich_vu = hd.ma_dich_vu
-join khach_hang kh
-on kh.ma_khach_hang = hd.ma_khach_hang
-join loai_khach lk
-on lk.ma_loai_khach = kh.ma_loai_khach
-order by hd.ma_hop_dong;
+-- 5.	Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, 
+-- tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet) 
+-- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 
 select kh.ma_khach_hang,kh.ho_ten,lk.ten_loai_khach, hd.ma_hop_dong,dv.ten_dich_vu, hd.ngay_lam_hop_dong,hd.ngay_ket_thuc, 
 (sum(dv.chi_phi_thue+ifnull(dvdk.gia*hdct.so_luong,0))) as tong_tien
